a38a780aee0b56fb23adb07920314ed12ecc3a32
Matt Rahmani <mmrahmani@yahoo.com>
2021-01-19 17:06:56 +1100

    Create bargain-finder-accessories.js

---
+++ test/specs/bargain-finder-accessories.js
@@ -0,0 +1,12 @@
+const SalePage = require('../pageobjects/sale.page');
+
+describe('David Jones bargain finder', () => {
+
+    it('should find bargains from Accessories category', () => {
+        SalePage.open('sale/bags-and-accessories');
+        SalePage.loadAllProducts();
+        SalePage.calculateDiscount();
+        SalePage.logSaleItems('Accessories');
+        console.log('=====>>> Accessories bargains search is finished <<<=====\n')
+    });
+});

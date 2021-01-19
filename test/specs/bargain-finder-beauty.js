10f8b2bee7a474e2597f2d5196241bedcccc3951
Matt Rahmani <mmrahmani@yahoo.com>
2021-01-19 17:06:54 +1100

    Create bargain-finder-beauty.js

---
+++ test/specs/bargain-finder-beauty.js
@@ -0,0 +1,12 @@
+const SalePage = require('../pageobjects/sale.page');
+
+describe('David Jones bargain finder', () => {
+
+    it('should find bargains from Beauty category', () => {
+        SalePage.open('sale/beauty');
+        SalePage.loadAllProducts();
+        SalePage.calculateDiscount();
+        SalePage.logSaleItems('Beauty');
+        console.log('=====>>> Beauty bargains search is finished <<<=====\n')
+    });
+});

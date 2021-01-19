8b15d90b46a4c1bc0c83409b859ed6cb6596d2c6
Matt Rahmani <mmrahmani@yahoo.com>
2021-01-19 17:06:49 +1100

    Create bargain-finder-kids.js

---
+++ test/specs/bargain-finder-kids.js
@@ -0,0 +1,12 @@
+const SalePage = require('../pageobjects/sale.page');
+
+describe('David Jones bargain finder', () => {
+
+    it('should find bargains from Kids category', () => {
+        SalePage.open('sale/kids');
+        SalePage.loadAllProducts();
+        SalePage.calculateDiscount();
+        SalePage.logSaleItems('Kids');
+        console.log('=====>>> Kids bargains search is finished <<<=====\n')
+    });
+});
